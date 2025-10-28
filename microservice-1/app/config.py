32f172a9412e4883575fb454215c67eceb4458ec
Alejandro Bogotá <l.bogotab@uniandes.edu.co>
2025-10-28 18:38:51 -0500

    [TEST]: New table testing

--- microservice-1/app/config.py
+++ microservice-1/app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
-    DDB_TABLE  = os.getenv("DDB_TABLE",  "medisupply-demo")
+    DDB_TABLE  = os.getenv("DDB_TABLE",  "medisupply-testing")
     # SQS
     SQS_QUEUE_URL = os.getenv("SQS_QUEUE_URL", "")
     ENABLE_SQS_PUBLISH = os.getenv("ENABLE_SQS_PUBLISH", "false").lower() == "true"
